04e7664d8df3cfe2e8897a08a6fe53f88bb9300f
Arsan <arsan@logique.co.id>
2021-10-16 08:22:58 +0700

    feat: Book Feature
    - Add Book Unit Test
    - Add and Update Book with validation

--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -2,20 +2,68 @@
 
 namespace Tests\Feature;
 
+use App\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class BookReservationTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling(); // disable laravel exception handling to see real error
+
+        $response = $this->post('/books',[
+            'title' => 'Test Book',
+            'author' => 'Test Author'
+        ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
+    }
+
+    /** @test */
+    public function a_title_is_required()
+    {
+        $response = $this->post('/books',[
+            'title' => '',
+            'author' => 'Test Author'
+        ]);
+
+        $response->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_author_is_required()
+    {
+        $response = $this->post('/books',[
+            'title' => 'Test Title',
+            'author' => ''
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/books',[
+            'title' => 'Test Title',
+            'author' => 'Test Author'
+        ]);
+
+        $book = Book::first();
+
+        $response = $this->patch('/books/' . $book->id,[
+            'title' => 'New Title',
+            'author' => 'New Author'
+        ]);
+
+        $this->assertEquals('New Title', Book::first()->title);
+        $this->assertEquals('New Author', Book::first()->author);
     }
 }
